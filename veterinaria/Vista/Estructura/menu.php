3b3469915609abb5d8f9418ab95ae1edcbfa161e
Gabriel Ignacio Cire <gabriel.cire@est.fi.uncoma.edu.ar>
2025-01-12 21:53:10 -0300

    commit-three

---
+++ veterinaria/Vista/Estructura/menu.php
@@ -0,0 +1,29 @@
+
+
+
+<!-- NAVBAR INICIO -->
+<nav class="navbar navbar-expand-lg  bg-success" >
+    <!-- INCIO MENÚ PÚBLICO -->
+    <div class="container-fluid m-3">
+        <a class="navbar-brand" href="../Vista/index.php">
+            <i class="fa-solid fa-seedling me-2" ></i>veterinaria<i class="fa-solid fa-seedling ms-2"></i>
+        </a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+               
+                <li class="nav-item">
+                    <a class="nav-link active" aria-current="page" href="../Vista/productos.php">Productos</a>
+                </li>
+            </ul>
+        
+        <!-- FIN MENÚ PÚBLICO -->
+        <ul class="navbar-nav ms-auto" id="menu">
+
+        </ul>
+        </div>
+    </div>
+</nav>
+<!-- NAVBAR FIN -->
